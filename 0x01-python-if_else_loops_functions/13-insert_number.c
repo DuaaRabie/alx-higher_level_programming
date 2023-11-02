5c64fbb663874dba47ff81376a095eadc76dc9aa
DuaaRabie <Duaarabie11@gmail.com>
2023-11-01 17:02:12 -0700

    update code

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -29,7 +29,7 @@ listint_t *insert_node(listint_t **head, int number)
 		return (new);
 	}
 	val = temp->next->n;
-	while (number > val && temp->next != NULL)
+	while (val < number && temp->next != NULL)
 	{
 		temp = temp->next;
 		val = temp->next->n;
