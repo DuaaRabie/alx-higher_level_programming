f2a0b4301b5872eb79749c2700842715487c0d4c
DuaaRabie <Duaarabie11@gmail.com>
2023-11-02 10:11:24 -0700

    update code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     length = len(sys.argv) - 1
     print("{} ".format(length), end="")
-    print("{}".format("argument" if length = 1 else "arguments"), end="")
+    print("{}".format("argument" if length == 1 else "arguments"), end="")
     if length > 0:
         print(":")
         for i in range(1, length + 1):
