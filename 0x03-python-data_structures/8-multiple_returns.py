a369976adf83b546430592e0fbc7782d47c4e2a0
DuaaRabie <Duaarabie11@gmail.com>
2023-11-06 07:43:59 -0800

    update code

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == "" or sentence == None:
+    if sentence == "" or sentence is None:
         first = None
     else:
         first = sentence[0]
