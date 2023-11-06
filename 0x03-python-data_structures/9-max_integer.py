fde72f5a001e95396b75a3bcd5c256bda780549a
DuaaRabie <Duaarabie11@gmail.com>
2023-11-06 08:10:27 -0800

    find max, task9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == [] or my_list is None:
+        return None
+    biggest = my_list[0]
+    for i in range(1, len(my_list)):
+        if biggest < my_list[i]:
+                biggest = my_list[i]
+    return biggest
