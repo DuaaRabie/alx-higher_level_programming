c161d061a3c24616b2bc33b914e56074944e0a3c
DuaaRabie <Duaarabie11@gmail.com>
2023-11-09 07:57:07 -0800

    roman integer, task12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    total = 0
+    prev_total = 0
+    if roman_string is not None and type(roman_string) is str:
+        for char in reversed(roman_string):
+            if char == 'I':
+                total += 1
+            elif char == 'V':
+                total += 5
+            elif char == 'X':
+                total += 10
+            elif char == 'L':
+                total += 50
+            elif char == 'C':
+                total += 100
+            elif char == 'D':
+                total += 500
+            elif char == 'M':
+                total += 1000
+            if total < prev_total:
+                total = total - 2 * prev_total
+            prev_total = total
+    return total
