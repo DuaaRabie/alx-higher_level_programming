0abb2c88e6c4a18352cd92c08f7c533197dba2fb
DuaaRabie <Duaarabie11@gmail.com>
2023-11-11 11:24:03 -0800

    update

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -13,7 +13,7 @@ void print_python_bytes(PyObject *p)
 	int i;
 
 	if (!(PyBytes_Check(p)))
-		printf("[ERROR] Invalid Bytes Object");
+		printf("[ERROR] Invalid Bytes Object\n");
 	else
 	{
 		(int)(PyBytes_AsStringAndSize(p, &firstbytes, &size));
@@ -29,4 +29,23 @@ void print_python_bytes(PyObject *p)
 		}
 	}
 	
+}
+
+/**
+ * print_python_list_info - prints python info
+ * @p: python object
+ * Return: nothing
+ */
+void print_python_list(PyObject *p)
+{
+	int i = 0, size = Py_SIZE(p);
+	int alloc = ((PyListObject *) p)->allocated;
+	PyObject *item;
+
+	printf("[*] Size of the Python List = %d\n[*] Allocated = %d\n", size, alloc);
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %d: %s\n", i, Py_TYPE(item)->tp_name);
+	}
 }
