92973a27fc3f0c9b8a15e0b43bc98873e7d8127d
DuaaRabie <Duaarabie11@gmail.com>
2023-11-20 16:23:08 -0800

    task0, safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for i in my_list:
+        try:
+            if i <= x:
+                print("{}".format(i), end="")
+                count += 1
+        except Exception:
+            break
+    print()
+    return count
