d9a0eefc7a7f116a5bb0944d0362724ed2031fe6
DuaaRabie <Duaarabie11@gmail.com>
2023-11-22 11:50:16 -0800

    update code

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -7,7 +7,8 @@ class MagicClass:
     def __init__(self, radius=0):
         if not isinstance(radius, (int, float)):
             raise TypeError('radius must be a number')
-        self._MagicClass__radius = radius
+        else:
+            self._MagicClass__radius = radius
         return None
 
     """This Method computes the area"""
