13d8ca85e36adca9e492f033ff2922d2d7a2ba3e
DuaaRabie <Duaarabie11@gmail.com>
2023-11-21 18:20:26 -0800

    update code

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,6 +3,7 @@
 
 
 class Square:
+    """ This Method defines a Square"""
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
