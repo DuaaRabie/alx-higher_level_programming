4ca001d019d3f8e1f6074dc0cc17f1999c8b8626
DuaaRabie <Duaarabie11@gmail.com>
2023-11-22 11:36:22 -0800

    task10, bytecode

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -3,11 +3,20 @@
 
 
 class MagicClass:
-    """This class """
-    def __init(self, radius):
+    """This class defines magic class"""
+    def __init__(self, radius):
         self._MagicClass__radius = 0
 
         if type(radius) is not int:
             raise TypeError('radius must be a number')
         else:
             self._MagicClass__radius = radius
+            return None
+
+    """This Method computes the area"""
+    def area(self):
+        return (self._MagicClass__radius ** 2) * math.pi
+
+    """This Method compute circumference"""
+    def circumference(self):
+        return 2 * math.pi * self._MagicClass__radius
