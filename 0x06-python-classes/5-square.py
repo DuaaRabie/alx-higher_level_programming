39e29ab5a7710dbec2ac2116cddb080cee3f102d
DuaaRabie <Duaarabie11@gmail.com>
2023-11-21 18:38:27 -0800

    task5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" This Module define class Square """
+
+
+class Square:
+    """ This Method defines a Square"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    """ This Method returns the current square area """
+    def area(self):
+        return self.__size ** 2
+
+    """ This Method prints in stdout the square with the character #"""
+    def my_print(self):
+        if self.__size == None:
+            print()
+        else:
+            for i in range(self.__size):
+                print(self.size * "#")
