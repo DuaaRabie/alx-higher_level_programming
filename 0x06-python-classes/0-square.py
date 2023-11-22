105c859df9306820c86e101ffff21f017c897dbd
DuaaRabie <Duaarabie11@gmail.com>
2023-11-21 17:52:42 -0800

    update code

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+""" This Module defines a class for square"""
 class Square:
-    """       This class defines a square, it is empty for now         """
+    """ This class defines a square, it is empty for now """
     
     pass
