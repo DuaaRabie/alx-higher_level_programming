cd42428ac6d489becdfd02b3215f8d0d683e8b8b
DuaaRabie <Duaarabie11@gmail.com>
2023-11-21 18:32:07 -0800

    update code

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,12 +5,20 @@
 class Square:
     """ This Method defines a Square"""
     def __init__(self, size=0):
-        if not isinstance(size, int):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            self.__size = value
 
     """ This Method returns the current square area """
     def area(self):
