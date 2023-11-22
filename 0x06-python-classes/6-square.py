67eabb171b900f7ff791da7f0c46e8a56942b918
DuaaRabie <Duaarabie11@gmail.com>
2023-11-21 18:48:23 -0800

    task6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,8 +4,9 @@
 
 class Square:
     """ This Method defines a Square"""
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=0):
         self.__size = size
+        self.__position = position
 
     @property
     def size(self):
@@ -19,6 +20,18 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
 
     """ This Method returns the current square area """
     def area(self):
