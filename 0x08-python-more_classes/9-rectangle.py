7ba58d59b514d19de6cc64e8c005ef5a54a5017d
DuaaRabie <Duaarabie11@gmail.com>
2023-11-27 16:20:16 -0800

    update code

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -81,5 +81,4 @@ class Rectangle():
 
     @classmethod
     def square(cls, size=0):
-        width, height = size, size
-        return cls(width, height)
+        return cls(width=size, height=size)
