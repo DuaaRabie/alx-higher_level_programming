72557945d31a2715fc3fcdb25750fa79ef1c133c
DuaaRabie <Duaarabie11@gmail.com>
2023-11-28 17:10:01 -0800

    update code

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(count=[0]):
-    count[0] += 1
-    return "BestSchool" * count[0]
+def magic_string(output=[]):
+    output.append("BestSchool")
+    return ", ".join(output)
