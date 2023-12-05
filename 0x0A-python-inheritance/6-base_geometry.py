ac2b8bc94573c80fee86d34b8e0a18e76cfb3080
DuaaRabie <Duaarabie11@gmail.com>
2023-12-04 18:55:30 -0800

    update code

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This module improve geomety"""
+
+
+class BaseGeometry():
+    """ This class to improve geomety"""
+    def area(self):
+        """ this function for area exception """
+        raise Exception("area() is not implemented")
