1aa57619bea93082c9d468c0c39bb9913b511ada
DuaaRabie <Duaarabie11@gmail.com>
2023-12-05 14:30:47 -0800

    task3, to json string

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ This module append to a file """
 
+
 def append_write(filename="", text=""):
     """ This function append to file """
     with open(filename, "a", encoding="utf-8") as f:
---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This Module represents to json """
+
+
+import json
+def to_json_string(my_obj):
+    """ This function returns json representation """
+    obj_str = json.dumps(my_obj)
+    return obj_str
