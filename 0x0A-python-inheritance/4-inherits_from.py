ce7358304a1363bc3a9dcda0067213a2d8727109
DuaaRabie <Duaarabie11@gmail.com>
2023-12-04 18:51:14 -0800

    update code

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,6 @@
 
 def inherits_from(obj, a_class):
     """ function to check inheritance """
-    return isinstance(obj, a_class)
+    if isinstance(obj, a_class) and type(obj) is not a_class:
+        return True
+    return False
