672b738fffaac0d6465bb2eecdf025b52d6f67b6
DuaaRabie <Duaarabie11@gmail.com>
2023-12-05 16:25:33 -0800

    task7, load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" This Module to load, add and save"""
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+args = sys.argv[1:]
+try:
+    data = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    data = []
+
+data += args
+save_to_json_file(data, "add_item.json")
