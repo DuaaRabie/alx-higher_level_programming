1c761fbc84b895f593c450237272d46e678f2118
DuaaRabie <Duaarabie11@gmail.com>
2023-12-06 06:36:14 -0800

    update task0, indentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
 """ This Module to read files """
 
 
 def read_file(filename=""):
     """ This function to read files """
     with open(filename, "r", encoding="utf-8") as f:
-    	for line in f:
+        for line in f:
             print(line, end="")
