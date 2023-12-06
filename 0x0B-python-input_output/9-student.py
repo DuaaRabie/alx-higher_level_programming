b0144184aabb384ccc66baa09901cd4a323d4889
DuaaRabie <Duaarabie11@gmail.com>
2023-12-05 17:44:01 -0800

    update code

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,9 +5,11 @@
 class Student():
     """ This class represents student """
     def __init__(self, first_name, last_name, age):
+        """ this function to initialize """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self):
+        """ This function retrieves a dictionary representation """
         return self.__dict__
