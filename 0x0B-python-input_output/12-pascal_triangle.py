ed90215f5b29360196696e6a142a1b24dea8b72e
DuaaRabie <Duaarabie11@gmail.com>
2023-12-05 18:15:41 -0800

    pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This module for pascal's triangle """
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    result = [[1]]
+    for i in range(1, n):
+        add = [result[i - 1][j - 1] + result[i -1][j] for j in range(1, i)]
+        row = [1] + add + [1]
+        result.append(row)
+    return result
