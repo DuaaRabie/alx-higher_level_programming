ee75bacdbbd5a80150cd7dbf921f66622bac3b1c
DuaaRabie <Duaarabie11@gmail.com>
2023-12-08 22:46:31 -0800

    update code

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -28,7 +28,7 @@ try:
                 if status_code_dic[i] > 0:
                     print("{}: {}".format(i, status_code_dic[i]))
 except KeyboardInterrupt:
-    print("File size: {}".format(total_size)
+    print("File size: {}".format(total_size))
     for i in sorted(status_code_dic.keys()):
         if status_code_dic[i] > 0:
             print("{}: {}".format(i, status_code_dic[i]))
