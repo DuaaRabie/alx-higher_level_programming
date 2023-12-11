39110425ac56ac37d2957e4800a34baa992e0d4f
DuaaRabie <Duaarabie11@gmail.com>
2023-12-11 01:05:59 -0800

    update code

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,10 +4,13 @@
 
 def matrix_divided(matrix, div):
     """ This function return the division of Matrix """
-    if type(matrix) is not list or\
-            (not all(type(x) is int for row in matrix for x in row) and \
-            not all(type(x) is float for row in matrix for x in row)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if type(matrix) is not list:
+        raise TypeError("matrix must be a matrix\
+                 (list of lists) of integers/floats")
+    if not all(type(x) is int for row in matrix for x in row) and\
+            not all(type(x) is float for row in matrix for x in row):
+        raise TypeError("matrix must be a matrix\
+                 (list of lists) of integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if not type(div) is int and not type(div) is float:
