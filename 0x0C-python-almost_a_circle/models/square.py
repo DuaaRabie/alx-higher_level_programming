691e86ebd4c092613062ee75584b3fa75bfaff3a
DuaaRabie <Duaarabie11@gmail.com>
2023-12-13 07:24:48 -0800

    create subclass square, task10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This module for square """
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ This class for Square """
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ This method represents the squar"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
