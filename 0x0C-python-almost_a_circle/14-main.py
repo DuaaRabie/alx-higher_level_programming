e3937b6343025187bd32a190d76ebd7d2a4ef009
DuaaRabie <Duaarabie11@gmail.com>
2023-12-14 14:15:32 -0800

    create a class method to write json string, task16

--- 0x0C-python-almost_a_circle/14-main.py
+++ 0x0C-python-almost_a_circle/14-main.py
@@ -2,13 +2,18 @@
 """ 14-main """
 from models.base import Base
 from models.rectangle import Rectangle
+from models.square import Square
 
 if __name__ == "__main__":
 
     r1 = Rectangle(10, 7, 2, 8)
-    dictionary = r1.to_dictionary()
-    json_dictionary = Base.to_json_string([dictionary])
-    print(dictionary)
-    print(type(dictionary))
+    r2 = Rectangle(10, 8, 3, 6)
+    dictionary1 = r1.to_dictionary()
+    dictionary2 = r2.to_dictionary()
+    json_dictionary = Base.to_json_string([dictionary1, dictionary2])
+    print(dictionary1)
+    print(type(dictionary1))
+    print(dictionary2)
+    print(type(dictionary2))
     print(json_dictionary)
     print(type(json_dictionary))
--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,6 +21,22 @@ class Base():
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """ This method returns json string representation """
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
-        return json.dumps(list_dictionaries)
+        to_conv = [
+                obj.to_dictionary() if not isinstance(obj, dict) else obj
+                for obj in list_dictionaries
+        ]
+        return json.dumps(to_conv)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ This method wrtie the json representation """
+        file_name = cls.__name__ + ".json"
+        if list_objs is None:
+            repre = "[]"
+        else:
+            repre = cls.to_json_string(list_objs)
+        with open(file_name, "w") as f:
+            f.write(repre)
