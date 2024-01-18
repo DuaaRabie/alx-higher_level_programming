f037b3954a4ca41215330d877b547b6ee4321a8b
Duaa Rabie <Duaarabie11@gmail.com>
2024-01-17 19:00:21 -0800

    update code

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,6 +2,7 @@
 SELECT tv_genres.name AS genre, COUNT(tv_shows.title) AS number_of_shows
 FROM tv_show_genres
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-GROUP BY tv_show_genres.genre_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_genres.name
 HAVING COUNT(tv_shows.title) > 0
 ORDER BY number_of_shows DESC;
