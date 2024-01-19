b63222ef2cb0bfa96ea42da9dc9840fc773a68fd
Duaa Rabie <Duaarabie11@gmail.com>
2024-01-18 18:47:28 -0800

    update code

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,8 +1,13 @@
---  lists all shows without the genre Comedy
+--  lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
 SELECT tv_shows.title
 FROM tv_show_genres
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id AND tv_genres.name = 'Comedy'
-WHERE tv_genres.name IS NULL
+WHERE NOT EXISTS (
+        SELECT *
+        FROM tv_show_genres
+        JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+        WHERE tv_show_genres.show_id = tv_shows.id AND tv_genres.name = 'Comedy'
+)
 GROUP BY tv_shows.title
 ORDER BY tv_shows.title ASC;
