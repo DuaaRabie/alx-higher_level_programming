3e005479c666b7801a73bf5a1b2e42ca3e3ab9a6
Duaa Rabie <Duaarabie11@gmail.com>
2024-01-18 17:25:22 -0800

    update code

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -2,6 +2,11 @@
 SELECT tv_genres.name
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id AND tv_shows.title = 'Dexter'
-WHERE tv_shows.title IS NULL
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.id != (
+	select tv_shows.id
+	from tv_shows
+	WHERE tv_shows.title = 'Dexter'
+)
+GROUP BY tv_genres.name
 ORDER BY tv_genres.name ASC;
