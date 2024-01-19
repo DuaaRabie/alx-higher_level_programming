bcfefa3135940da489eb5238eecf354f91fb7455
Duaa Rabie <Duaarabie11@gmail.com>
2024-01-18 18:25:52 -0800

    update code

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -2,7 +2,7 @@
 SELECT tv_shows.title
 FROM tv_show_genres
 LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name != 'Comedy'
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id AND tv_genres.name = 'Comedy'
+WHERE tv_genres.name IS NULL
 GROUP BY tv_shows.title
 ORDER BY tv_shows.title ASC;
