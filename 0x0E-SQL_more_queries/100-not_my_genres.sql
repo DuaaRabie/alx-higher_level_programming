4dc3dea195c60a1d5a1455ef6e9649ef6fff55c1
Duaa Rabie <Duaarabie11@gmail.com>
2024-01-18 17:49:16 -0800

    update code

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -3,10 +3,11 @@ SELECT tv_genres.name
 FROM tv_show_genres
 LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_show_genres.show_id != (
-	SELECT tv_shows.id
-	FROM tv_shows
-	WHERE tv_shows.title = 'Dexter'
+WHERE NOT EXISTS (
+	SELECT *
+	FROM tv_show_genres
+	JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+	WHERE tv_show_genres.genre_id = tv_genres.id AND tv_shows.title = 'Dexter'
 )
 GROUP BY tv_genres.name
 ORDER BY tv_genres.name ASC;
