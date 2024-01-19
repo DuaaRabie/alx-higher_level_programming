bcf60b29fdf1ec6209a0cd4b956b625c9d5ab2fc
Duaa Rabie <Duaarabie11@gmail.com>
2024-01-18 17:43:21 -0800

    update code

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,9 +1,9 @@
--- list all genres not linked to the show Dexter
+-- Lists all genres not linked to the show Dexter
 SELECT tv_genres.name
-FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+FROM tv_show_genres
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.id != (
+WHERE tv_show_genres.show_id != (
 	SELECT tv_shows.id
 	FROM tv_shows
 	WHERE tv_shows.title = 'Dexter'
