5bf0167c595efa0d6f875cbde4fd2eaee1e6a760
duaa rabie <duaarabie11@gmail.com>
2024-02-12 17:04:18 -0800

    update code

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,11 +3,11 @@
 const process = require('process');
 
 if (process.argv[2]) {
-	const converted = Number(process.argv[2]);
+  const converted = Number(process.argv[2]);
 
-	if (!isNaN(converted)) {
-		console.log("My number:", converted);
-	} else {
-		console.log('Not a number');
-	};
+  if (!isNaN(converted)) {
+    console.log('My number:', converted);
+  } else {
+    console.log('Not a number');
+  }
 }
