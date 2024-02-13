88eb209d970c90671cbb8be7e3730fe7fc698938
duaa rabie <duaarabie11@gmail.com>
2024-02-12 17:29:25 -0800

    update code

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,9 +4,10 @@ const args = process.argv;
 
 if (args[2]) {
   const first = Number(args[2]);
+
   if (!isNaN(first)) {
     for (let i = 0; i < first; i++) {
-    console.log('C is fun');
+      console.log('C is fun');
     }
   } else {
     console.log('Missing number of occurrences');
