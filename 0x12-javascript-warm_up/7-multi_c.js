aaae36f3fb865250e4d53e5d2750be4c110acbab
duaa rabie <duaarabie11@gmail.com>
2024-02-12 17:18:17 -0800

    task7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 const args = process.argv;
-let first = Number(process.argv[2]);
+let first = Number(args[2]);
 
 if (first) {
-	while (first) {
-		console.log("C is fun");
-		first = first - 1;
-	}
+  while (first) {
+    console.log('C is fun');
+    first = first - 1;
+  }
 } else {
-	console.log("Missing number of occurrences");
+  console.log('Missing number of occurrences');
 }
