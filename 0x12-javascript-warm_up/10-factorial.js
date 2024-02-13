14400aff621d6194d58edc8c1bd2ab6857ae80f1
duaa rabie <duaarabie11@gmail.com>
2024-02-12 18:02:14 -0800

    task10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+const args = process.argv;
+let factorial = 1;
+
+if (isNaN(Number(args[2])) || Number(args[2]) === 0) {
+  factorial = 1;	
+} else {
+	for (i = Number(args[2]); i > 0; i--) {
+	  factorial *= i;
+	}
+}
+console.log(factorial);
