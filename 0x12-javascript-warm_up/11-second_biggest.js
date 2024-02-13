169c5d649ac397db4833027e2136f789ce0e54da
duaa rabie <duaarabie11@gmail.com>
2024-02-12 18:44:41 -0800

    update code

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,16 +7,15 @@ if (args.length === 2) {
 } else if (args.length === 3) {
   console.log(0);
 } else {
-	let biggest = args[3];
-	for (let i = 4; i < args.length; i++) {
+  let biggest = args[3];
+  for (let i = 4; i < args.length; i++) {
 	  if (args[i] > biggest) {
 	    biggest = args[i];
 	  }
-	}
-        let secbig = args[3];
-	for (i = 4; i < args.length; i++) {
-	  if (args[i] > secbig && args[i] < biggest)
-		secbig = args[i];
-	}
-	console.log(secbig);
+  }
+  let secbig = args[3];
+  for (i = 4; i < args.length; i++) {
+	  if (args[i] > secbig && args[i] < biggest) { secbig = args[i]; }
+  }
+  console.log(secbig);
 }
