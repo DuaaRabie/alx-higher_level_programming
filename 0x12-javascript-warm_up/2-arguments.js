7251b33811bbebb3b872726665d1e38a0ddacc82
duaa rabie <duaarabie11@gmail.com>
2024-02-12 16:11:07 -0800

    update code

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
-const args = process.argv.length - 2;
+const args = process.argv.length;
 
-if (args === 0) {
+if (args === 2) {
 	console.log('No argument');
-} else if (args === 1) {
+} else if (args === 3) {
 	console.log('Argument found');
 } else {
 	console.log('Arguments found');
 }
+
