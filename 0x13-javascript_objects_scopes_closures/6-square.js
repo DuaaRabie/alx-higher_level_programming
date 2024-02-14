293a05e4947a4ca4def0fb43a0cd7c08b3893b6d
duaa rabie <duaarabie11@gmail.com>
2024-02-13 18:24:50 -0800

    update code

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,22 +7,6 @@ class Square extends Rectangle {
     super(size, size);
   }
 
-  charPrint (c) {
-    let result = '';
-
-    if (c === undefined) {
-      c = 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        result += c;
-      }
-      if (i < this.height - 1) {
-        result += '\n';
-      }
-    }
-    console.log(result);
-  }
 }
 
 module.exports = Square;
