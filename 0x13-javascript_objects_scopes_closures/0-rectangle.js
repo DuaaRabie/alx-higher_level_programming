f34779dd3eac8567b17f42d8728b443cd8cffddf
duaa rabie <duaarabie11@gmail.com>
2024-02-13 16:36:19 -0800

    task0

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+/*
+ * an empty class Rectangle that defines a rectangle
+ * must use the class notation for defining your class
+ */
+
+class Rectangle {
+}
+module.exports = Rectangle
