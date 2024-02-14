ddd56d937832f7cf93bce0c2bc7ab04c9f3b77be
duaa rabie <duaarabie11@gmail.com>
2024-02-13 18:28:33 -0800

    update code

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,27 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const mainSquare = require('./5-square.js');
 
-class Square extends Rectangle {
+class Square extends mainSquare {
   constructor (size) {
     super(size, size);
   }
-  charPrint(c) {
+
+  charPrint (c) {
+    let result = '';
+
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        result += c;
+      }
+      if (i < this.height - 1) {
+        result += '\n';
+      }
+    }
+    console.log(result);
   }
 }
 
