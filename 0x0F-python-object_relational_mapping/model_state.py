bd24fc4cf5abb6a80c2d7fa3b6dfa332b4be4109
Duaa Rabie <Duaarabie11@gmail.com>
2024-02-18 18:46:44 -0800

    update code

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -14,11 +14,19 @@ if __name__ == "__main__":
             charset="utf8")
 
     cursor = db.cursor()
-    cursor.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    query = "SELECT cities.name\
+            FROM cities JOIN states ON cities.state_id = states.id\
+            WHERE states.name = %s\
+            ORDER BY cities.id ASC"
+    cursor.execute(query, (sys.argv[4],))
 
     rows = cursor.fetchall()
     for row in rows:
-        print(row)
+        for element in row:
+            print(element, end='')
+            if row != rows[(len(rows) - 1)]:
+                print(", ", end="")
+    print()
 
     cursor.close()
     db.close()
