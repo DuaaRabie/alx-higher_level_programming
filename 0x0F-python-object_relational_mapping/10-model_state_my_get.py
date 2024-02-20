ed5d9f0cf7e2ceace99638350a2bf5624e171315
Duaa Rabie <Duaarabie11@gmail.com>
2024-02-20 00:37:06 -0800

    task10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Module for states select """
+from sqlalchemy import create_engine, MetaData, Integer, \
+        String, Column, ForeignKey
+from sqlalchemy.orm import relationship, Session
+from sqlalchemy.ext.declarative import declarative_base
+import sys
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    name = sys.argv[1]
+    pd = sys.argv[2]
+    db = sys.argv[3]
+    engform = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(name, pd, db)
+    engine = create_engine(engform)
+    Base.metadata.create_all(engine)
+    session = Session(bind=engine)
+    states = session.query(State).filter(State.name.like(sys.argv[4])).all()
+
+    if states:
+        for state in states:
+            print("{}".format(state.id))
+    else:
+        print("Not found")
