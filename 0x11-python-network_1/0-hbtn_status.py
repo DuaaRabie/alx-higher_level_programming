17848c912223db5567e3c0b7b197def588240e1e
Duaa Rabie <DuaaRabie11@gmail.com.com>
2024-03-01 15:42:38 -0800

    task0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" This Module fetches alx intranet """
+import urllib.request
+
+
+url = "https://alx-intranet.hbtn.io/status"
+req = urllib.request.Request(url)
+with urllib.request.urlopen(req) as response:
+    contents = response.read()
+    contents_utf8 = contents.decode('utf-8')
+    print("Body response:")
+    print("    - type: {}".format(type(contents)))
+    print("    - content: {}".format(contents))
+    print("    - utf8 content: {}".format(contents_utf8))
