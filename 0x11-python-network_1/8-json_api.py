9797da3cb33ce20de5870b51093b67f68e82955f
Duaa Rabie <DuaaRabie11@gmail.com.com>
2024-03-01 17:28:47 -0800

    task8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,14 +2,24 @@
 """ This Module sends request to url and display value """
 import sys
 import requests
+import json
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) > 1:
+        letter = sys.argv[2]
+    else:
+        letter = ""
+    data = {"q": letter}
+    response = requests.post(url, data)
     try:
-        response = requests.get(url)
-        contents = response.text
-        print(contents)
-    except urllib.error.HTTPError as error:
-        if error.code >= 400:
-            print("Error code: {}".format(error.code))
+        res_json = response.json()
+    except requests.exceptions.JSONDecodeError as e:
+        print("Not a valid JSON")
+
+    if not res_json:
+        print("No result")
+    else:
+        print("[{}] {}".format(res_json.get("id"), res_json.get("name")))
+
