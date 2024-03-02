bc10c48f4ae6fb469f7091fa92484468d48c2b74
Duaa Rabie <DuaaRabie11@gmail.com.com>
2024-03-01 18:02:37 -0800

    task10

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" This Module sends request to url and display value """
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://developer.github.com/v3/repos/commits/"
+    repository_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    auth = (repository_name, owner_name)
+    response = requests.post(url, auth=auth)
+    try:
+        res_json = response.json()
+    except requests.exceptions.JSONDecodeError as e:
+        print("Not a valid JSON")
+        sys.exit(1)
+
+    if not res_json:
+        print("No result")
+    else:
+        print("{}: {}".format(res_json.get("sha"), res_json("author_name")))
