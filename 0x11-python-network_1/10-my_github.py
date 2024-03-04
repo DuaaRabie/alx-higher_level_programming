51b41b8bf087fbbe3b4c0e4333c0b40c084c7e6b
Duaa Rabie <DuaaRabie11@gmail.com.com>
2024-03-04 06:37:54 -0800

    update code

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
     password = sys.argv[2]
     url = f"https://api.github.com/users/{username}"
     headers = {'Authorization': f'password {password}'}
-    try:
-        response = requests.post(url, headers=headers)
-        res_json = response.json()
+    response = requests.post(url, headers=headers)
+    res_json = response.json()
+    if response.status_code == 200:
         print(res_json.get("id"))
-    except requests.exceptions.HTTPError as err:
+    else:
         print("None")
