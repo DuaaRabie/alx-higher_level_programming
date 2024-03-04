dc3b3a003bb3e4133bfc4f60a46b6e458948d231
Duaa Rabie <DuaaRabie11@gmail.com.com>
2024-03-04 06:21:52 -0800

    update code

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     password = sys.argv[2]
     url = f"https://api.github.com/users/{username}"
     headers = {'Authorization': f'password {password}'}
-    response = requests.post(url, headers=headers)
+    response = requests.get(url, headers=headers)
     res_json = response.json()
     if response.status_code == 200:
         print("None")
