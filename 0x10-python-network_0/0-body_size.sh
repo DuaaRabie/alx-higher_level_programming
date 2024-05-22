5f2206f96e06a86136c4a96b786c5a1c5cc677b1
Duaa Rabie <DuaaRabie11@gmail.com>
2024-05-22 12:19:42 -0700

    task0

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#curl body size
+
+if [ $# -lt 1 ]; then
+	echo "usage: $0 <URL>"
+	exit 1
+fi
+response=$(curl -sI "$1")
+content_length=$(echo "$response" | grep -oP '(?<=Content-Length: ).*')
+echo "$content_length"
