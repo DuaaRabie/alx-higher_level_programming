4b21fa661e80bc6b9645b504a12676450ccbf605
Duaa Rabie <DuaaRabie11@gmail.com>
2024-05-22 12:23:44 -0700

    updaet code

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 #curl body size
-
-if [ $# -lt 1 ]; then
-	echo "usage: $0 <URL>"
-	exit 1
-fi
-response=$(curl -sI "$1")
-content_length=$(echo "$response" | grep -oP '(?<=Content-Length: ).*')
-echo "$content_length"
+echo $(curl -sI "$1" | grep -oP '(?<=Content-Length: ).*')
