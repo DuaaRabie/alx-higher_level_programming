c08845f9442396b37ae36e0b3c0c3071943eac69
Duaa Rabie <DuaaRabie11@gmail.com>
2024-05-23 10:21:59 -0700

    update code

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #curl Post parameters
-curl -s -d 'email: "test@gmail.com"' -d 'subject: I will always be here for PLD' -X POST "$1"
+curl -s -X POST "$1" -d "email=test@gmail.com&subject=I will always be here for PLD"
