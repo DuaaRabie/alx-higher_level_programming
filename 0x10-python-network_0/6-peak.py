210ccb02367620df3355ea321433668221dd783e
Duaa Rabie <DuaaRabie11@gmail.com>
2024-05-25 23:54:07 -0700

    update code

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,18 +9,20 @@ def find_peak(list_of_integers):
 
     while left <= right:
         mid = (left + right) // 2
+        middle = list_of_integers[mid]
+        before = list_of_integers[mid - 1]
+        after = list_of_integers[mid + 1]
 
-    # Check if the middle element is greater than its neighbors
-    if mid == 0 or list_of_integers[mid] > list_of_integers[mid - 1]:
-        if (mid == len(list_of_integers) - 1) or \
-                (list_of_integers[mid] > list_of_integers[mid + 1]):
+        # Check if the middle element is greater than its neighbors
+        if ((mid == 0) or (middle > before)) and \
+                ((mid == len(list_of_integers) - 1) or (middle > after)):
             return list_of_integers[mid]
-    # If the middle element is not a peak,
-    # move towards the side where the neighbor is larger
-    elif mid != 0 and list_of_integers[mid - 1] > list_of_integers[mid]:
-        right = mid - 1
-    else:
-        left = mid + 1
+        # If the middle element is not a peak,
+        # move towards the side where the neighbor is larger
+        elif mid != 0 and list_of_integers[mid - 1] > list_of_integers[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
 
     # Return None if no peak is found
     return None
